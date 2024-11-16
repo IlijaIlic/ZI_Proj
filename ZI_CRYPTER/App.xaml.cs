eae683e02f86261624f59d68e864e473300370b3
Ilija Ilic <ilija.ilic@elfak.rs>
2024-11-16 13:12:42 +0100

    Transition to WPF

---
+++ ZI_CRYPTER/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ZI_CRYPTER
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
